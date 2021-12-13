c4e6382511add23c9a1b408d1e9a0d62a7f9a2a4
chisambwe <c.m.kapihya@gmail.com>
2021-12-13 19:56:30 +0000

    User Story 3
    As an air traffic controller
    To ensure safety
    I want to prevent landing when the airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,13 @@
 class Airport
+
+    def initialize(capacity)
+        @capacity = capacity
+        @planes = []
+    end
+
     def land(plane)
+        raise "Cannot land plane: airport is full. Take off a plane first." if @planes.length >= @capacity
+        @planes << plane
     end
 
     def take_off(plane)
